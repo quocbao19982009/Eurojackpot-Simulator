92c828c01520e6af56f0cbb3dcb5277e64c59603
Bao Nguyen <86527891+quocbao19982009@users.noreply.github.com>
2022-02-03 15:40:51 +0200

    adding logic in lottery win con

---
+++ backend/ultis/calculatedWin.js
@@ -0,0 +1,82 @@
+const numberSelectedbyUser = [1, 2, 3, 18, 50];
+const numberSelectedResult = [1, 3, 20, 23, 25];
+const starNumberSelectedbyUser = [1, 2];
+const starNumberSelectedResult = [2, 3];
+
+const inputLottery = {
+  numberSelectedbyUser,
+  starNumberSelectedbyUser,
+};
+
+const resultLottery = {
+  numberSelectedResult,
+  starNumberSelectedResult,
+};
+/* playerLottery: {
+numberSelectedbyUser,
+numberSelectedResult
+}
+*/
+
+const compareLotteryTicket = (arr1, arr2) => {
+  return arr1.filter((item) => arr2.includes(item));
+};
+
+const calculatedWin = (playerLottery, resultLottery) => {
+  const { number: numberSelectedbyUser, starNumber: starNumberSelectedbyUser } =
+    playerLottery;
+  const { number: numberSelectedResult, starNumber: starNumberSelectedResult } =
+    resultLottery;
+
+  const winNumber = compareLotteryTicket(
+    numberSelectedbyUser,
+    numberSelectedResult
+  ).length;
+  const winStartNumber = compareLotteryTicket(
+    starNumberSelectedbyUser,
+    starNumberSelectedResult
+  ).length;
+
+  if (winNumber === 2 && winStartNumber === 1) {
+    return 8;
+  }
+
+  if (winNumber === 1 && winStartNumber === 2) {
+    return 10;
+  }
+
+  if (winNumber === 3 && winStartNumber === 0) {
+    return 15;
+  }
+  if (winNumber === 3 && winStartNumber === 1) {
+    return 17;
+  }
+  if (winNumber === 2 && winStartNumber === 2) {
+    return 20;
+  }
+
+  if (winNumber === 4 && winStartNumber === 0) {
+    return 103;
+  }
+
+  if (winNumber === 3 && winStartNumber === 2) {
+    return 56;
+  }
+
+  if (winNumber === 4 && winStartNumber === 1) {
+    return 229;
+  }
+
+  if (winNumber === 4 && winStartNumber === 2) {
+    return 4016;
+  }
+
+  if (winNumber === 5 && winStartNumber === 0) {
+    return 97566;
+  }
+  if (winNumber === 5 && winStartNumber === 2) {
+    return 33334010;
+  }
+};
+
+export default calculatedWin;
